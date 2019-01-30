eea86d46f8ec549793fb4efd8216ba7e8438c4d9
Eden Chambers <EdenChambers5@gmail.com>
2019-01-30 13:18:25 -0700

    I added some explanation comments, made the array, made a way to copy the array, and made the destructor.

--- CPPStructuresProject/Model/Linear/Array.hpp
+++ CPPStructuresProject/Model/Linear/Array.hpp
@@ -14,7 +14,7 @@
 
 using namespace std;    //Uded for keyword access.
 
-template <class Type>
+template <class Type>   //Allows it to work with any type
 class Array
 {
 private:
@@ -40,7 +40,33 @@ public:
     void setAtIndex(int index, Type data);
 };
 
+template <class Type>
+Array<Type> :: Array(int size)
+{
+    assert(size > 0);
+    this->size = size;
+    
+    internalArray = new Type[size];
+}
 
+template <class Type>
+Array<Type> :: Array(const Array<Type> & toCopy)
+{
+    this->size = toCopy.getSize();
+    
+    //Build Data Structure
+    internalArray = new Type[size];
+    
+    for(int index = 0; index < size; inde++)
+    {
+        internalArray[index] = toCopy[index];
+    }
+}
 
+template <class Type>
+Array<Type> :: ~Array() //The ~ is for destructors only and cannot be overridden
+{
+    delete [] internalArray;
+}
 
 #endif /* Array_h */
