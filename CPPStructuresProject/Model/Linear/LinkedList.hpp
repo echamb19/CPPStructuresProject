becfcf14fee015703520247a6a5e46486b3c44eb
Eden Chambers <EdenChambers5@gmail.com>
2019-02-07 13:10:27 -0700

    I added the LinkedList file and did the using namespace std thing.

---
+++ CPPStructuresProject/Model/Linear/LinkedList.hpp
@@ -0,0 +1,19 @@
+//
+//  LinkedList.hpp
+//  CPPStructuresProject
+//
+//  Created by Chambers, Eden on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef LinkedList_h
+#define LinkedList_h
+
+#include "List.hpp"
+
+using namespace std;    // Used for keyword access.
+// ^ It makes sure My pointers are pointing to the right thing.
+
+
+
+#endif /* LinkedList_h */
