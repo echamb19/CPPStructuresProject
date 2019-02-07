8a16355f9e3247e5898817dbdda38e633e064b48
Eden Chambers <EdenChambers5@gmail.com>
2019-02-07 13:16:00 -0700

    I made the class and declared the variables and methods that would be necessary.

--- CPPStructuresProject/Model/Linear/LinkedList.hpp
+++ CPPStructuresProject/Model/Linear/LinkedList.hpp
@@ -14,6 +14,32 @@
 using namespace std;    // Used for keyword access.
 // ^ It makes sure My pointers are pointing to the right thing.
 
+template <class Type>
+class LinkedList : public List<Type>
+{
+protected:
+    LinearNode<Type> * front;
+    LinearNode<Type> * end;
+    
+public:
+    // Constructors
+    LinkedList();
+    // Destructor
+    virtual ~LinkedList();  // Virtual so it can be overridden.
+    // Helper Methods
+    int getSize() const;
+    LinearNode<Type> * getFront();
+    LinearNode<Type> * getEnd();
+    
+    // Structure Methods
+    void add(Type item);
+    void addAtIndex(int index, Type item);
+    Type getFromIndex(int index);
+    type remove(int index);
+//    Type setAtIndex(int index, Type item);
+//    bool contains(Type item);
+};
+
 
 
 #endif /* LinkedList_h */
