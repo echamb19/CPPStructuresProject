ee9a517a6b8c3604abb0e36eddeda2ed072bdd96
Eden Chambers <EdenChambers5@gmail.com>
2019-02-11 13:33:28 -0700

    I added the enqueue, addAtIndex, and add methods.
    I also added the destructor.

--- CPPStructuresProject/Model/Linear/Queue.hpp
+++ CPPStructuresProject/Model/Linear/Queue.hpp
@@ -33,6 +33,52 @@ public:
     Type remove(int index);
 };
 
+template <class Type>
+Queue<Type> :: Queue() : LinkedList<Type>()
+{
+    // Empty because it is handled by the parent class constructor
+}
+
+template<class Type>
+Queue<Type> :: ~Queue()
+{
+    for(LinearNode<Type> * removed = this-> front; removed != nullprt; removed = this->front)
+    {
+        this->front = removed->getNextNode();
+        delete removed;
+    }
+}
+
+template<class Type>
+void Queue<Type> :: enqueue(Type item)
+{
+    LinearNode<Type> * added = new LinearNode<Type>(item);
+    
+    if(this->size == 0)
+    {
+        this->front = added;
+    }
+    else
+    {
+        this->end->setNextNode(added);
+    }
+    this->end = added;
+    this->size++;
+}
+
+template<class Type>
+void Queue<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index == this->size);
+    enqueue(item);
+}
+
+template <class Type>
+void Queue<Type> :: add(Type item)
+{
+    enqueue(item);
+}
+
 
 
 #endif /* Queue_h */
