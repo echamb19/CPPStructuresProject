aeace3c1f9353d3595bb9a322781b5ea5cf9f49c
Eden Chambers <EdenChambers5@gmail.com>
2019-02-12 11:49:37 -0700

    I added the stack.hpp.

---
+++ CPPStructuresProject/Model/Linear/Stack.hpp
@@ -0,0 +1,14 @@
+//
+//  Stack.hpp
+//  CPPStructuresProject
+//
+//  Created by Chambers, Eden on 2/11/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Stack_h
+#define Stack_h
+
+
+
+#endif /* Stack_h */
