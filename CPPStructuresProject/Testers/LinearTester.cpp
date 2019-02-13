1dcfe61e02934b8d75fcf079867fce675f590599
Eden Chambers <EdenChambers5@gmail.com>
2019-02-13 13:51:51 -0700

    I added the testVsSTL() method.

--- CPPStructuresProject/Testers/LinearTester.cpp
+++ CPPStructuresProject/Testers/LinearTester.cpp
@@ -8,4 +8,36 @@
 
 #include "LinearTester.hpp"
 
-
+void LinearTester :: testVsSTL()
+{
+    Timer crimeTimerSTL, crimeTimerOOP, musicSTL, musicOOP;
+    
+    crimeTimerSTL.startTimer();
+    vector<CrimeData> crimes = FileController :: readCrimeDataToVector("/Users/echa3557/Documents/C++ Projects/CPPStructuresProject/CPPStructuresProject/Resources/crime.csv");
+    crimeTimerSTL.stopTimer();
+    
+    crimeTimerOOP.startTimer();
+    LinkedList<CrimeData> moreCrimes = FileController :: readDataToList("/Users/echa3557/Documents/C++ Projects/CPPStructuresProject/CPPStructuresProject/Resources/crime.csv");
+    crimeTimerOOP.stopTimer();
+    
+    crimeTimerSTL.displayInformation();
+    crimeTimerOOP.displayInformation();
+    cout << "A difference of: " << crimeTimerOOP.getTimeInMicroseconds() - crimeTimerSTL.getTimeInMicroseconds() << " microseconds" << endl;
+    
+    musicSTL.startTimer();
+    vector<Music> tunez = FileController :: musicDataToVector("/Users/echa3557/Documents/C++ Projects/CPPStructuresProject/CPPStructuresProject/Resources/music.csv");
+    musicSTL.stopTimer();
+    
+    musicOOP.startTimer();
+    LinkedList<Music> musicList = FileController :: musicDataToList("/Users/echa3557/Documents/C++ Projects/CPPStructuresProject/CPPStructuresProject/Resources/music.csv");
+    musicOOP.stopTimer();
+    
+    musicSTL.displayInformation();
+    musicOOP.displayInformation();
+    cout << "A difference of: " << musicOOP.getTimeInMicroseconds() - musicSTL.getTimeInMicroseconds() << " microseconds." << endl;
+    
+    crimeTimerOOP.resetTimer();
+    crimeTimerSTL.resetTimer();
+    musicOOP.resetTimer();
+    musicSTL.resetTimer();
+}
