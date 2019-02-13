72e905b786ca0dca5534521493cb7717fb92c1f1
Eden Chambers <EdenChambers5@gmail.com>
2019-02-13 12:40:01 -0700

    I made the class declaration and the cconstructor and destructor. I also began the push method.

--- CPPStructuresProject/Model/Linear/Stack.hpp
+++ CPPStructuresProject/Model/Linear/Stack.hpp
@@ -9,6 +9,46 @@
 #ifndef Stack_h
 #define Stack_h
 
+#include "LinkedList.hpp"
 
+template <class Type>
+class Stack : public LinkedList<Type>
+{
+public:
+    Stack();
+    ~Stack();
+    
+    // Stack Specific Methods
+    void push(Type data);
+    Type pop();
+    Type peek();
+    
+    // Overridden LinkedList Methods
+    void add(Type data);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    Type remove(int index)
+};
+
+template <class Type>
+Stack<Type> :: Stack() : LinkedList<Type>()
+{
+    
+}
+
+template <class Type>
+Stack<Type> :: ~Stack()
+{
+    while(this->size > 0)
+    {
+        pop();
+    }
+}
+
+template <class Type>
+void Stack<Type> :: push(Type data)
+{
+    
+}
 
 #endif /* Stack_h */
