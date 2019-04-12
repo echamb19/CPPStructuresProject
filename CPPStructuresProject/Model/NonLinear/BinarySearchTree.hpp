b4c3c25bc318940e86ed64ebbd26fa654104e955
Eden Chambers <EdenChambers5@gmail.com>
2019-04-12 13:56:50 -0600

    BinarySearchTree exists.

---
+++ CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
@@ -0,0 +1,23 @@
+//
+//  BinarySearchTree.hpp
+//  CPPStructuresProject
+//
+//  Created by Chambers, Eden on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinarySearchTree_h
+#define BinarySearchTree_h
+
+#include "Tree.hpp"
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+class BinarySearchTree : public Tree<Type>
+{
+    
+};
+
+#endif /* BinarySearchTree_h */
