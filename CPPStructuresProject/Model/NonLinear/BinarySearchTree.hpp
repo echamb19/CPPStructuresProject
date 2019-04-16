99f2538b69cb1d8715a4a40c58946ed8943156f7
Eden Chambers <EdenChambers5@gmail.com>
2019-04-16 12:21:35 -0600

    I got the informational method stubs and the traversal stubs done.

--- CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
+++ CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
@@ -56,9 +56,48 @@ public:
     
     Type findMinimum();
     Type findMaximum();
-    
 };
 
+template <class Type>
+int BinarySearchTree<Type> :: getHeight()
+{
+    return -1;
+}
 
+template <class Type>
+int BinarySearchTree<Type> :: getSize()
+{
+    return -1;
+}
+
+template <class Type>
+bool BinarySearchTree<Type> :: isComplete()
+{
+    return false;
+}
+
+template <class Type>
+bool BinarySearchTree<Type> :: isBalanced()
+{
+    return false;
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: inOrderTraversal()
+{
+    
+}
+
+template <class Type>
+void BinarySearchTree<Type> :: preOrderTraversal()
+{
+    
+}
+
+template<class Type>
+void BinarySearchTree<Type> :: postOrderTraversal()
+{
+    
+}
 
 #endif /* BinarySearchTree_h */
