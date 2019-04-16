bb9897a52eddeac8560b9aa0d52fb20525902a04
Eden Chambers <EdenChambers5@gmail.com>
2019-04-16 12:05:40 -0600

    I added the methods to the class declaration.

--- CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
+++ CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
@@ -17,7 +17,22 @@ using namespace std;
 template <class Type>
 class BinarySearchTree : public Tree<Type>
 {
+protected:
+    int calculateSize(BinaryTreeNode<Type> * startNode);
+    int calculateHeight(BinaryTreeNode<Type> * startNode);
+    bool isBalanced(BinaryTreeNode<Type> * startNode);
+    bool isComplete(BinaryTreeNode<Type> * StartNode, int index, int size);
     
+    void inOrderTraversal(BinaryTreeNode<Type> * inStart);
+    void preOrderTraversal(BinaryTreeNode<Type> * preStart);
+    void postOrderTraversal(BinaryTreeNode<Type> * postStart);
+    
+    void destroyTree(BinaryTreeNode<Type> * node);
+    
+    BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
+    BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
+    
+    void removeNode(BinaryTreeNode<Type> * removeMe); 
 };
 
 #endif /* BinarySearchTree_h */
