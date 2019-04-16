619ef3bb14c9b2eee6ce9ca460ea9c5886de33a3
Eden Chambers <EdenChambers5@gmail.com>
2019-04-16 12:11:32 -0600

    I added the public stuff to my class declaration.

--- CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
+++ CPPStructuresProject/Model/NonLinear/BinarySearchTree.hpp
@@ -32,7 +32,33 @@ protected:
     BinaryTreeNode<Type> * getRightMostChild(BinaryTreeNode<Type> * current);
     BinaryTreeNode<Type> * getLeftMostChild(BinaryTreeNode<Type> * current);
     
-    void removeNode(BinaryTreeNode<Type> * removeMe); 
+    void removeNode(BinaryTreeNode<Type> * removeMe);
+    
+public:
+    BinarySearchTree();
+    ~BinarySearchTree();
+    
+    BinaryTreeNode<Type> * getRoot();
+    
+    void inOrderTraversal();
+    void preOrderTraversal();
+    void postOrderTraversal();
+    void demeoTraversalSteps(BinaryTreeNode<Type> node);
+    
+    int getSize();
+    int getHeight();
+    bool isComplete();
+    bool isBalanced();
+    
+    bool contains(Type value);
+    void insert(Type itemToInsert);
+    void remove(Type value);
+    
+    Type findMinimum();
+    Type findMaximum();
+    
 };
+
+
 
 #endif /* BinarySearchTree_h */
